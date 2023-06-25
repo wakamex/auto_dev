b2ed34cda5b621a0f5131030515a566f6e735e25
8baller <tomrae030@hotmail.com>
2023-06-25 21:55:31 +0100

    [feat] added in templates for readme scaffolding

--- auto_dev/commands/repo.py
+++ auto_dev/commands/repo.py
@@ -9,16 +9,16 @@ contains the following commands;
         . pyproject.toml
 
 """
+import importlib
 from dataclasses import dataclass
-
-# we will use a class based approach to scaffold the repos, so we can use it in multiple places
+from glob import glob
 from pathlib import Path
-from typing import List
+from typing import Dict, List
 
 import rich_click as click
 
 from auto_dev.base import build_cli
-from auto_dev.constants import DEFAULT_ENCODING
+from auto_dev.constants import DEFAULT_ENCODING, TEMPLATE_FOLDER
 
 cli = build_cli()
 
@@ -38,8 +38,13 @@ keys.json
 class ScaffoldFile:
     """Class to store a file to scaffold."""
 
-    name: str
     content: str
+    path: Path = None
+
+    @property
+    def name(self):
+        """Get the name of the file."""
+        return Path(self.path).stem + Path(self.path).suffix
 
 
 @dataclass
@@ -47,7 +52,7 @@ class RepoType:
     """Class to store a repo type."""
 
     name: str
-    files: List[ScaffoldFile]
+    files: Dict[str, ScaffoldFile]
 
 
 class RepoScaffolder:
@@ -66,8 +71,13 @@ class RepoScaffolder:
         repo_type = SUPPORTED_REPO_TYPES[type_of_repo]
         for filen in files:
             if filen not in repo_type.files:
-                raise ValueError(f"File not supported: {filen}" + f"Files must be one of: {repo_type.files}")
-            self.check_and_write_file(filen, "")
+                raise ValueError(
+                    f"Files must be one of: `{list(repo_type.files.keys())}`. Howerver, `{filen}` was given."
+                )
+            new_file_generator = repo_type.files[filen]
+            new_file_name = new_file_generator.name
+            new_file_content = new_file_generator.content
+            self.check_and_write_file(new_file_name, new_file_content)
 
     def check_and_write_file(self, path, content):
         """Check if a file exists and write it if it doesn't."""
@@ -79,27 +89,51 @@ class RepoScaffolder:
         path.write_text(content, encoding=DEFAULT_ENCODING)
 
 
-PYTHON_REPO_TYPE = RepoType(
-    name="python",
-    files=[
-        ScaffoldFile(name=".gitignore", content=PYTHON_GIT_IGNORE),
-        ScaffoldFile(name="README.md", content=""),
-        ScaffoldFile(name="pyproject.toml", content=""),
-        ScaffoldFile(name=".github/workflows/ci.yml", content=""),
-    ],
-)
-
-OPEN_AUTONOMY_REPO_TYPE = RepoType(
-    name="open-autonomy",
-    files=[
-        ScaffoldFile(name=".gitignore", content=OPEN_AUTONOMY_GIT_IGNORE),
-    ],
-)
+def test_get_supported_repo_types():
+    """Test the get_supported_repo_types function."""
+    assert get_supported_repo_types({}) == SUPPORTED_REPO_TYPES
 
 
-SUPPORTED_REPO_TYPES = {
-    PYTHON_REPO_TYPE.name: PYTHON_REPO_TYPE,
+def get_supported_repo_types(render_args) -> dict:
+    """
+    Get the supported repo types from the templates folder.
+    We will do this by reading in from the libraries folders;
+    """
+    del render_args
+
+    supported_repos = {}
+
+    for template_folder in Path(TEMPLATE_FOLDER).glob("*"):
+        files = glob(str(template_folder / "**/*"), recursive=True)
+        scaffold_files = {}
+        for filep in files:
+            # our files are as .py files so we will need to import them
+            # TypeError: the 'package' argument is required to perform a relative import
+            # we will need to import the file and then call the render function
+            if not filep.endswith(".py"):
+                # we need to remove the .py from the end of the file
+                continue
+
+            template_path = Path(filep)
+            import_path = f"auto_dev.data.repo.templates.{template_path.parent.name}.{template_path.stem}"
+            template = importlib.import_module(import_path)
+            file_output = template.TEMPLATE
+            output_path = Path(template_path.stem + template.EXTENSION)
+            scaffold_file = ScaffoldFile(path=output_path.name, content=file_output)
+            scaffold_files[template_path.stem] = scaffold_file
+        supported_repos[template_folder.name] = RepoType(name=template_folder, files=scaffold_files)
+    return supported_repos
+
+
+render_args = {
+    "project_name": "test",
+    "author": "8ball030",
+    "email": "8ball030@gmail.com",
+    "description": "test",
+    "version": "0.1.0",
 }
+
+SUPPORTED_REPO_TYPES = get_supported_repo_types(render_args)
 
 
 @cli.group()
@@ -122,9 +156,11 @@ def scaffold(ctx, type_of_repo, files):
     logger = ctx.obj["LOGGER"]
     verbose = ctx.obj["VERBOSE"]
     logger.info(f"Scaffolding a new {type_of_repo} repo.")
+    if not Path(".git").exists():
+        raise ValueError("Not in the top level of a git repo.")
     repo_type = SUPPORTED_REPO_TYPES[type_of_repo]
     if files == ():
-        raise ValueError("No files provided." + f"Files must be one of: {[f.name for f in repo_type.files]}")
+        raise ValueError(f"No files provided. Must be one of: {list(repo_type.files.keys())}")
     scaffolder = RepoScaffolder(verbose, logger)
     scaffolder.scaffold(type_of_repo=type_of_repo, files=files)
     logger.info("Done.")
--- auto_dev/constants.py
+++ auto_dev/constants.py
@@ -10,4 +10,6 @@ DEFAULT_ENCODING = "utf-8"
 PACKAGE_DIR = os.path.dirname(os.path.abspath(__file__))
 DEFAULT_PYLAMA_CONFIG = Path(PACKAGE_DIR) / "data" / "pylama.ini"
 AUTONOMY_PACKAGES_FILE = "packages/packages.json"
-PLUGIN_FOLDER = os.path.join(os.path.dirname(__file__), 'commands')
+AUTO_DEV_FOLDER = os.path.join(os.path.dirname(__file__))
+PLUGIN_FOLDER = os.path.join(AUTO_DEV_FOLDER, "commands")
+TEMPLATE_FOLDER = os.path.join(AUTO_DEV_FOLDER, "data", "repo", "templates")
