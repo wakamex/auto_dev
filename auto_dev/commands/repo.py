2004f8ed0b86ac7a8a176f07131e9fb730173004
Karrenbelt <m.a.p.karrenbelt@gmail.com>
2023-09-21 22:10:40 +0200

    chore: add python_versions to render kwargs

--- auto_dev/commands/repo.py
+++ auto_dev/commands/repo.py
@@ -136,12 +136,18 @@ def get_supported_repo_types(render_args) -> dict:
     return supported_repos
 
 
+def format_versions(versions: list[str]) -> str:
+    version_prefix = " " * 8 + "- "
+    return f"\n{version_prefix}".join(versions)
+
+
 render_args = {
     "project_name": "test",
     "author": "8ball030",
     "email": "8ball030@gmail.com",
     "description": "test",
     "version": "0.1.0",
+    "python_versions": format_versions(["3.7", "3.8", "3.9", "3.10"]),
 }
 
 SUPPORTED_REPO_TYPES = get_supported_repo_types(render_args)
