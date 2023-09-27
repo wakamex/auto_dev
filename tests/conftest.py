6f320d56dde90975f483230d87f3b85899dd41ea
Karrenbelt <m.a.p.karrenbelt@gmail.com>
2023-09-27 19:39:24 +0200

    test: fixture dummy_agent_tim

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,9 +2,13 @@
 Conftest for testing command-line interfaces.
 """
 
+import os
+from pathlib import Path
+
 import pytest
 from click.testing import CliRunner
 
+from auto_dev.cli_executor import CommandExecutor
 from auto_dev.utils import isolated_filesystem
 
 
@@ -26,3 +30,16 @@ def test_clean_filesystem():
     """Fixture for invoking command-line interfaces."""
     with isolated_filesystem() as directory:
         yield directory
+
+
+@pytest.fixture
+def dummy_agent_tim(test_clean_filesystem) -> None:
+    """Fixture for dummy agent tim."""
+    assert Path.cwd() == Path(test_clean_filesystem)
+
+    command = CommandExecutor(["aea", "create", "tim"])
+    result = command.execute(verbose=True)
+    if not result:
+        raise ValueError("Failed to create dummy agent tim")
+
+    os.chdir(str(Path.cwd() / "tim"))
