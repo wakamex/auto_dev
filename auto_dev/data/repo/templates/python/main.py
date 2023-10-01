c48cbde6b916185acc14b3e88703a4770fb3e3f2
Karrenbelt <m.a.p.karrenbelt@gmail.com>
2023-10-01 19:11:24 +0200

    fix: docstring to main.py

--- auto_dev/data/repo/templates/python/main.py
+++ auto_dev/data/repo/templates/python/main.py
@@ -4,6 +4,7 @@ Template file for the the main.py file
 """
 EXTENSION = ".py"
 TEMPLATE = """
+\"\"\"Main module for your Python application.\"\"\"
 print("hello world!")
 """
 DIR = "./{project_name}/"
