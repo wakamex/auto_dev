2f156c5017703f80407676411552345a302f270c
Karrenbelt <m.a.p.karrenbelt@gmail.com>
2023-10-01 19:11:31 +0200

    fix: docstring to test_main.py

--- auto_dev/data/repo/templates/python/test_main.py
+++ auto_dev/data/repo/templates/python/test_main.py
@@ -4,8 +4,9 @@ Template file for the the main.py file
 """
 EXTENSION = ".py"
 TEMPLATE = """
-
+\"\"\"Test cases for the main module.\"\"\"
 def test_main():
+    \"\"\"Test main\"\"\"
     assert True
 """
 DIR = "./test/"
