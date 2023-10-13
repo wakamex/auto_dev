3f735fa72de6343f725d7c2296bfd540caf4d587
Karrenbelt <m.a.p.karrenbelt@gmail.com>
2023-10-13 18:45:17 +0200

    tests: autonomy .gitignore

--- tests/test_repo.py
+++ tests/test_repo.py
@@ -10,6 +10,7 @@ import pytest
 
 from auto_dev.cli import cli
 from auto_dev.utils import change_dir
+from aea.cli.utils.config import get_default_author_from_cli_config
 
 
 class BaseTestRepo:
@@ -83,8 +84,46 @@ class TestRepoPython(BaseTestRepo):
 class TestRepoAutonomy(BaseTestRepo):
     """Test scaffolding new autonomy repository."""
 
+    author = get_default_author_from_cli_config()
     type_of_repo = "autonomy"
     make_commands = "fmt", "test", "lint", "hashes"
+
+    def test_gitignore(self, cli_runner, test_clean_filesystem):
+        """Test the .gitignore works as expected"""
+
+        assert test_clean_filesystem
+
+        result = cli_runner.invoke(cli, self.cli_args)
+        assert result.exit_code == 0, result.output
+
+        packages_folder = self.repo_path / "packages"
+        author_packages = packages_folder / self.author
+        author_packages.mkdir(parents=True)
+
+        # create files that should be ignored in the authors folder
+        for folder in ("protocols", "connections", "skills", "agents", "services"):
+            subfolder = author_packages / folder
+            subfolder.mkdir()
+            (subfolder / "keys.json").write_text("SECRET")
+            (subfolder / "my_private_keys").write_text("SECRET")
+            (subfolder / "__pycache__").mkdir()
+            (subfolder / "__pycache__" / "cachefile").write_text("cache data")
+
+        # any packages not from self should be ignored
+        another_author = packages_folder / "another_author"
+        another_author.mkdir()
+        (another_author / "some_file").write_text("some content")
+
+        with change_dir(self.repo_path):
+            result = subprocess.run(['git', 'status', '--porcelain'], capture_output=True, text=True, check=False)
+            assert result.returncode == 0
+            assert "packages" not in result.stdout
+
+            # any other file created in the author's own package directory should be detected
+            (author_packages / "some_other_file").write_text("to_be_committed")
+            result = subprocess.run(['git', 'status', '--porcelain'], capture_output=True, text=True, check=False)
+            assert result.returncode == 0
+            assert "packages" in result.stdout
 
     def test_run_single_agent(self, cli_runner, test_clean_filesystem):
         """Test the scripts/run_single_agent.sh is generated"""
