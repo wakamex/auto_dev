bbd58fd0bba4bcaf5d92b868499e500a2ed40377
8baller <tomrae030@hotmail.com>
2023-10-14 19:48:03 +0100

    [fix] bumping

--- auto_dev/commands/release.py
+++ auto_dev/commands/release.py
@@ -48,7 +48,7 @@ class Releaser:
         """
         We update the version.
         """
-        command = f"tbump --new-version {new_version}"
+        command = f"tbump {new_version}"
         self.logger.info(f"Running command:\n `{command}`")
 
         cli_tool = CommandExecutor(
