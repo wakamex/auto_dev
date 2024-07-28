5cf37683d6056a9717c9e9bc29a7dc155d20c7e9
8baller <tomrae030@hotmail.com>
2024-07-28 02:26:56 +0000

    fix: resolved small issue

--- auto_dev/test.py
+++ auto_dev/test.py
@@ -1,6 +1,7 @@
 """
 Module for testing the project.
 """
+import sys
 from .cli_executor import CommandExecutor
 
 
@@ -9,6 +10,7 @@ def test_path(path: str, verbose: bool = False) -> bool:
     Check the path for linting errors.
     :param path: The path to check
     """
+    available_cores = sys.cpu_count()
     command = CommandExecutor(
         [
             "poetry",
@@ -16,6 +18,8 @@ def test_path(path: str, verbose: bool = False) -> bool:
             "pytest",
             str(path),
             "-vv",
+            "-n",
+            str(available_cores),
         ]
     )
     result = command.execute(verbose=verbose, stream=True)
