91f85e988dd43ce7bb380a436213b084c5c859d4
8baller <tomrae030@hotmail.com>
2024-08-04 21:27:20 +0000

    checkpoint: ensure scripts run

--- scripts/check_generated.sh
+++ scripts/check_generated.sh
@@ -1,4 +1,5 @@
 REPO_NAME="test_repo"
 poetry run adev repo $REPO_NAME -t autonomy
 cd $REPO_NAME
+poetry lock --no-update && poetry install
 cp poetry.lock ../auto_dev/data/repo/templates/autonomy/
