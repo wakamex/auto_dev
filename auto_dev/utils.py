cdba2faa3ee9f028b8eb63deaafa8e050404860b
8baller <tomrae030@hotmail.com>
2024-08-08 11:38:22 +0000

    chore: silly linter

--- auto_dev/utils.py
+++ auto_dev/utils.py
@@ -32,7 +32,7 @@ def get_logger(name=__name__, log_level="INFO"):
     )
     # We set the time to just the 24 hours minutes and seconds
     datefmt = "%H:%M:%S"
-    logging.basicConfig(level="NOTSET", 
+    logging.basicConfig(level="NOTSET",
                         datefmt=datefmt,
                         handlers=[handler])
 
