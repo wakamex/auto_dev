b93a9a36ec559faf89922b91d3d95659bbd1daa3
8ball030 <8baller@station.codes>
2024-09-05 14:02:03 +0000

    feat:-checkpoint-@-Thu  5 Sep 14:02:03 UTC 2024

--- auto_dev/behaviours/scaffolder.py
+++ auto_dev/behaviours/scaffolder.py
@@ -63,7 +63,7 @@ class BehaviourScaffolder(ProtocolScaffolder):
         template = self.env.get_template(str(Path(self.component_class) / f"{self.behaviour_type.value}.jinja"))
         protocol_specification = read_protocol(self.protocol_specification_path)
         output = template.render(
-            protocol_name=protocol_specification.name,
+            protocol_name=protocol_specification.metadata['name'],
             year=datetime.datetime.now(currenttz()).year,
         )
         if self.verbose:
