758b680e68b701ec09f6d7de61568c605a582e59
8ball030 <8baller@station.codes>
2025-01-12 17:29:35 +0000

    Bump to 0.2.85

--- auto_dev/commands/deps.py
+++ auto_dev/commands/deps.py
@@ -408,7 +408,7 @@ open_aea_repo = GitDependency(
 
 auto_dev_repo = GitDependency(
     name="autonomy-dev",
-    version="0.2.84",
+    version="0.2.85",
     location=DependencyLocation.REMOTE,
     url="https://api.github.com/repos/8ball030/auto_dev",
     extras=["all"],
